f630cd279fd4dd25aaebf354bb527f5b7d7703ce
Jaime <burbanojaime98@gmail.com>
2022-07-27 20:53:59 -0500

    mobile version partial finished

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,4 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
-Alpine.store('shop', {
-  name: 'Alpine-Shop',
-  products: ['Swiss Alp Chocolate', 'Car Alpine A110'],
-})
-
 Alpine.start()
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.html",
   ],
   theme: {
-    extend: {},
+    colors: {
+      black: '#000000',
+      emerald: '#001211',
+      white: '#FFFFFF',
+      yellow: '#FFC627',
+      blue: '#00818A',
+      grass: '#142524'
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif']
+    },
+    extend: {
+
+    },
   },
   plugins: [],
 }
