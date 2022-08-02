fd5cf094b352648b22f31ed81c3b19fee8496059
Jaime <burbanojaime98@gmail.com>
2022-08-01 22:28:28 -0500

    layout finished

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,5 +25,10 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+    },
+    require('tailwind-children'),
+  ],
 }
